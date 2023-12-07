b3152fa56e407c2d7c8fde5d059a64bb21b8825e
Victor <vitorsntossouza9866@gmail.com>
2023-12-06 22:00:27 -0300

    montando a estrutura da aplicacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomLayoutComponent } from './views/custom-layout/custom-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: CustomLayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/custom-layout/custom-layout.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { CustomLayoutComponent } from './custom-layout.component';
+
+describe('CustomLayoutComponent', () => {
+  let component: CustomLayoutComponent;
+  let fixture: ComponentFixture<CustomLayoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CustomLayoutComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustomLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
